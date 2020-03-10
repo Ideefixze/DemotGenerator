5f8b825a7bc9c1dfad6030cd4c0f1b5280cfef48
Clockworker <44556744+Ideefixze@users.noreply.github.com>
2020-03-10 23:39:45 +0100

    AutoDemot

--- src/pyquests.py
+++ src/pyquests.py
@@ -32,29 +32,82 @@ print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
 '''
 import io
 import os
+import praw as p
+import requests 
+from tqdm import tqdm
 
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
 
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+def Labeling():
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = 'D:\\pyquests\\src\\image4.jpg'
+    # The name of the image file to annotate
+    file_name = 'D:\\pyquests\\src\\image4.jpg'
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
 
-image = types.Image(content=content)
+    image = types.Image(content=content)
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description + " " + str(label.topicality))
+    print('Labels:')
+    for label in labels:
+        print(label.description + " " + str(label.topicality))
+
+
+def ZrobDemota():
+    URL='http://www.demotmaker.com.pl/zaawansowane.php'
+    PARAMS={'action':'lang=pl','obrazek':'2','tekst':'nowydemot','kolor1':'Biały',
+    'kolor2':'Biały','rozmiar1':'36','rozmiar2':'16','opis':'test','plik':'','link':'https://i.redd.it/o5ni0epffwl41.jpg',
+    'obrobka':'brak','obrot':'brak','kolorobr':'Czarne','rozmiarobr':'2','kolortla':'Czarne'}
+    r = requests.post(url = URL, data=PARAMS, files = PARAMS)
+    #print(r.request.body)
+    #print(r.request.headers)
+    #print(r.request.)
+    #print(r.text)
+    start = r.text.find("obrazki/")
+    end = r.text.find("\"",start)
+    print(start)
+    print(end)
+
+    url_part=r.text[start:end]
+
+    img_response = requests.get("http://demotmaker.com.pl/"+url_part, stream=True)
+    with open("D:\\pyquests\\src\\dls\\demot.jpg", "wb") as handle:
+        for data in tqdm(img_response.iter_content()):
+            handle.write(data)
+    
+def TEST():
+    URL='http://ptsv2.com/t/Joseph/post'
+    PARAMS={'aaa':'aaa'}
+    r = requests.post(url = URL, data = PARAMS)
+    print(r.text)
+
+def SaveToFile(filename, content):
+    with open("D:\\pyquests\\src\\dls\\"+filename+".jpg", "wb") as handle:
+        for data in tqdm(content.iter_content()):
+            handle.write(data)
+
+def RedditTitles():
+    reddit = p.Reddit(client_id='yd5bYoeRUp7Gfg',client_secret='WWLeGwXepDBVvFYucp6Y4x6hMO8',user_agent='myagent')
+    print(reddit.read_only)
+    
+    for submission in reddit.subreddit('hmmm').hot(limit=50):
+        
+        url = submission.url
+        print(submission.url)
+        #response = requests.get(url, stream=True)
+        #SaveToFile(str(submission),response)
+        
+#TEST()
+ZrobDemota()
+#RedditTitles()
 
