2b800d190ccb2f3020b62c9db679626ba1401355
Clockworker <44556744+Ideefixze@users.noreply.github.com>
2020-04-06 22:28:59 +0200

    Revert "Update pyquests.py"
    This reverts commit 29cb2d0841235158c2f28216db2fb77e9100920b.

--- src/pyquests.py
+++ src/pyquests.py
@@ -66,6 +66,7 @@ def GetRandomLink():
     r = img_links[random.randint(0,len(img_links)-1)]
     return r
 
+
 print("How many demots you want to generate?")
 howmany = int(input())
 #Generates demots
@@ -73,6 +74,6 @@ print("All demots will be saved in "+current_dir+"/dls/")
 RedditTitles(min(howmany*50,4000))
 for i in range(1, howmany):
     GenerateDemot()
-print("Operation has been completed!")
+print("Operation completed!")
 
 
